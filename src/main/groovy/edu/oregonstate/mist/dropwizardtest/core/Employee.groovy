21ffc480b667bb4b121201159b005ecc5a1a8190
Taylor Brown <browtayl@onid.oregonstate.edu>
2015-02-05 14:50:32 -0800

    remove unused imports

--- src/main/groovy/edu/oregonstate/mist/dropwizardtest/core/Employee.groovy
+++ src/main/groovy/edu/oregonstate/mist/dropwizardtest/core/Employee.groovy
@@ -4,8 +4,6 @@ import javax.persistence.Entity
 import javax.persistence.Id
 import javax.persistence.Table
 import javax.persistence.Column
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
 
 @Entity
 @Table(name='PYVPASE')
