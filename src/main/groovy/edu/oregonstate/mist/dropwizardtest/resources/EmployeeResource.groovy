99452bf2767836449b490573f8f60581debd51f4
Taylor Brown <browtayl@onid.oregonstate.edu>
2015-02-05 12:40:10 -0800

    respond with 404 if requested employee does not exist

--- src/main/groovy/edu/oregonstate/mist/dropwizardtest/resources/EmployeeResource.groovy
+++ src/main/groovy/edu/oregonstate/mist/dropwizardtest/resources/EmployeeResource.groovy
@@ -14,7 +14,7 @@ import javax.ws.rs.Path
 import javax.ws.rs.PathParam
 import javax.ws.rs.Produces
 import javax.ws.rs.Consumes
-//import javax.ws.rs.NotFoundException
+import javax.ws.rs.WebApplicationException
 import javax.ws.rs.core.MediaType
 import javax.ws.rs.core.Response
 //import javax.ws.rs.core.Response.Status
@@ -43,8 +43,8 @@ public class EmployeeResource {
 
         if (employee.isPresent()) {
             return employee.get()
-        //} else {
-            //throw new NotFoundException("No such employee.")
+        } else {
+            throw new WebApplicationException(Response.Status.NOT_FOUND)
         }
     }
 
