d24f99508068d5e3f9682107680c126a94ef4055
Taylor Brown <browtayl@onid.oregonstate.edu>
2015-02-18 15:51:25 -0800

    groovyify employee class members

--- src/main/groovy/edu/oregonstate/mist/dropwizardtest/core/Employee.groovy
+++ src/main/groovy/edu/oregonstate/mist/dropwizardtest/core/Employee.groovy
@@ -13,68 +13,32 @@ import org.hibernate.validator.constraints.Email
 public class Employee {
     @Id
     @Column(name='PYVPASE_PIDM')
-    private final Long id
+    final Long id
 
     @Column(name='PYVPASE_ID')
     @NotEmpty
-    private String osuId
+    String osuId
 
     @Column(name='PYVPASE_LAST_NAME')
     @NotEmpty
-    private String lastName
+    String lastName
 
     @Column(name='PYVPASE_FIRST_NAME')
-    private String firstName
+    String firstName
 
     @Column(name='PYVPASE_MI')
-    private String middleInitial
+    String middleInitial
 
     @Column(name='PYVPASE_ONID_LOGIN')
-    private String onidLoginId
+    String onidLoginId
 
     @Column(name='PYVPASE_EMAIL')
     @Email
-    private String emailAddress
+    String emailAddress
 
     @Column(name='PYVPASE_EMPL_STATUS')
     @NotEmpty
-    private String employeeStatus
-
-    public Employee() {
-    }
-
-    /**
-     * Employee Constructor
-     *
-     * Instantiate an Employee object with given id and initialize its
-     * properties to null.
-     *
-     * @param id
-     *            unique identifier
-     * @return new Employee object
-     */
-    public Employee(Integer id) {
-        this.id = id
-    }
-
-    /* getters */
-    public Integer getId() { return id }
-    public String getOsuId() { return osuId }
-    public String getLastName() { return lastName }
-    public String getFirstName() { return firstName }
-    public String getMiddleInitial() { return middleInitial }
-    public String getOnidLoginId() { return onidLoginId }
-    public String getEmailAddress() { return emailAddress }
-    public String getEmployeeStatus() { return employeeStatus }
-
-    /* setters */
-    public void setOsuId(String osuId) { this.osuId = osuId }
-    public void setLastName(String lastName) { this.lastName = lastName }
-    public void setFirstName(String firstName) { this.firstName = firstName }
-    public void setMiddleInitial(String middleInitial) { this.middleInitial = middleInitial }
-    public void setOnidLoginId(String onidLoginId) { this.onidLoginId = onidLoginId }
-    public void setEmailAddress(String emailAddress) { this.emailAddress = emailAddress }
-    public void setEmployeeStatus(String employeeStatus) { this.employeeStatus = employeeStatus }
+    String employeeStatus
 
     /**
      * Override equals
